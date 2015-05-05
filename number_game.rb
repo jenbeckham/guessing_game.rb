6e4b0172320f4cd7c312dcfde5aa6430840e69ea
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-05 15:44:50 -0400

    Working loop

--- number_game.rb
+++ number_game.rb
@@ -1,12 +1,12 @@
 puts "guess what number I am thinking between 1 and 100?"
 
 x = rand(1..100)
-user_input = gets.chomp.to_i
 
 def computer_num(x)
-  if user_input > x
+  user_input = gets.chomp.to_f
+  if x < user_input
     puts "To High!"
-  elsif user_input < x
+  elsif x > user_input
     puts "To Low!"
   else
     puts "You got it right, HOORRAYY!!"
@@ -14,8 +14,8 @@ def computer_num(x)
 end
 
 counter = 5
-while counter != 0
+until counter == 1
   counter = counter - 1
   computer_num(x)
 end
-puts "Game Over"
+puts "Game Over. The number is #{x}"
