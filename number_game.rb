fe8a99ef6279e6a43b4a2159e2c0dc6994218e6e
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-05 17:17:22 -0400

    Added working array

--- number_game.rb
+++ number_game.rb
@@ -2,8 +2,17 @@ puts "guess what number I am thinking between 1 and 100?"
 
 x = rand(1..100)
 
-def computer_num(x)
-  user_input = gets.chomp.to_f
+A = []
+def check_number(user_input)
+  if A.include?(user_input)
+    puts "Did you bump your head?"
+  else
+    A << user_input
+  end
+end
+
+def computer_num(x, user_input)
+  # user_input = gets.chomp.to_f
   if x < user_input
     puts "To High!"
   elsif x > user_input
@@ -14,8 +23,11 @@ def computer_num(x)
 end
 
 counter = 5
-until counter == 1
+until counter == 0
+  user_input = gets.chomp.to_f
   counter = counter - 1
-  computer_num(x)
+  computer_num(x, user_input)
+  check_number(user_input)
 end
-puts "Game Over. The number is #{x}"
+
+puts "Game Over. The number is #{x}."
